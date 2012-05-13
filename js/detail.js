062121fd243fce0c7cbbd0c37fd1a7f548661343
Michelle Lee <meeshellee@gmail.com>
2012-05-13 14:16:17 -0400

    added tag pages

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,6 @@
 (function(GLOBAL, $, tmpl) {
   
-  var content, tag_list, new_tag;
+  var content, new_tag;//, tag_list;
 
   $.parse.init({
       app_id: "LKndLqOkr8fE4AH7BcfpmJbXNJMW6ElrMPHGI7iY", // <-- enter your Application Id here
@@ -11,7 +11,7 @@
     content = $("#scroller");
 	tagfield = $("#tagfield");
 	
-	tag_list = $("#tag_list");
+	//tag_list = $("#tag_list");
     tag_table = $("#tag_table");
 	
     _addEvents();
@@ -43,25 +43,27 @@
 	
 	$.parse.get("pieces", {where : { objectId: id }}, function(json) {
 		console.log("get objectId: ", json.results[0].objectId);
-		console.log("get pieceName: ", json.results[0].pieceName);
+		console.log("get title: ", json.results[0].title);
 		tagCounts = json.results[0].tagCounts;
 		console.log("tagCounts ", tagCounts);
 		
 		var tagExists = -1;
 		if (tagCounts.hasOwnProperty(val)) {
 			tagExists = 0;
-			console.log("tag exists");
+			console.log("piece already has tag");
 		}
 		
 		if (tagExists < 0) { // if piece does NOT have this tag
 			console.log("tag does not exist");
 			// set count of tag to 1
-			tagCounts[val] = 1;
+			// tagCounts[val][0] = count, [val][1] = tagId
+			tagCounts[val] = [];
+			tagCounts[val][0] = 1;
 			
 			// update piece tags to newtags
-			$.parse.put("pieces/" + id, { tagCounts: tagCounts }, function(json) {
+			/*$.parse.put("pieces/" + id, { tagCounts: tagCounts }, function(json) {
 				console.log('updated piece tags ' + id, json)
-			});
+			});*/
 			
 			// check if tag exists in Tags
 			$.parse.get("tags", {where : {"tagName": val}}, function(json) {
@@ -70,20 +72,28 @@
 				// if Tags does not contain tag, add tag
 				if (results.length == 0) {
 					console.log("tag not in Tags");
-					tag = $(tmpl("tag_tmpl", { objectId: false, tagName: val, pieceNames: [id] }));
-					tag_list.append(tag);
-					console.log("added to tag_list");
+					//tag = $(tmpl("tag_tmpl", { objectId: false, tagName: val, pieceIds: [id] }));
+					//tag_list.append(tag);
+					//console.log("added to tag_list");
 					
-					 $.parse.post('tags', { tagName: val, pieceNames: [id] }, function(json) { 
-						tag.data('id', json.objectId);
+					$.parse.post('tags', { tagName: val, pieceIds: [id] }, function(json) { 
+						//tag.data('id', json.objectId);
 						console.log("new tagid ", json.objectId);
 						console.log("new tagid ", json.createdAt);
+						
+						// add tagId to tagCounts
+						tagCounts[val][1] = json.objectId;
+						console.log("tag[1]", tagCounts[val][1]);
+						$.parse.put("pieces/" + id, { tagCounts: tagCounts }, function(json) {
+							console.log('updated piece tags ' + id, json)
+						});
 					});
 					console.log("posted");
+					
 				}
 				else { // else add piece to tag
 					console.log("tag is in Tags");
-					oldpieces = json.results[0].pieceNames;
+					oldpieces = json.results[0].pieceIds;
 					console.log("oldpieces ", oldpieces);
 					
 					newpieces = oldpieces.concat(id);
@@ -91,15 +101,21 @@
 					
 					tagId = json.results[0].objectId;
 					
-					$.parse.put("tags/" + tagId, { pieceNames: newpieces }, function(json) {
+					$.parse.put("tags/" + tagId, { pieceIds: newpieces }, function(json) {
 						console.log('updated tag pieces' + id, json)
 					});
+					
+					tagCounts[val][1] = tagId;
+					$.parse.put("pieces/" + id, { tagCounts: tagCounts }, function(json) {
+						console.log('updated piece tags ' + id, json)
+					});
 				}
+				
 			});
 		}
 		else { // if the piece already has this tag
 			// piece's tag count ++
-			tagCounts[val] = tagCounts[val] + 1;
+			tagCounts[val][0] = tagCounts[val][0] + 1;
 			
 			// update piece tags to new tags
 			$.parse.put("pieces/" + id, { tagCounts: tagCounts }, function(json) {
@@ -108,7 +124,9 @@
 		}
 		var newHTML = "";
 		for (var key in tagCounts) {
-			newHTML += '<span class="tag tag-'+tagCounts[key]+'">'+key+'</span>'
+			newHTML += '<a href="tag.html?i='+tagCounts[key][1]+'">';
+			newHTML += '<span class="tag tag-'+tagCounts[key][0]+'">'+key+'</span>';
+			newHTML += '</a>';
 		}
 		document.getElementById("tags").innerHTML = newHTML;
 		myScroll.refresh();
---
+++ js/tags.js
@@ -0,0 +1,62 @@
+(function(GLOBAL, $, tmpl) {
+  
+  var piece_list, piece_detail; 
+
+  $.parse.init({
+      app_id: "LKndLqOkr8fE4AH7BcfpmJbXNJMW6ElrMPHGI7iY", // <-- enter your Application Id here
+      rest_key: "wIpqcITCvjSirwsYUkGco27hXMoNPmiBHzuulflw" // <--enter your REST API Key here	
+  });
+
+  function init() {
+    piece_list = $("#scroller");
+    piece_detail = $("#piece_detail");
+    return GLOBAL.pieces;
+  }
+
+  function get() {
+    var id = objURL[ "i" ];
+	
+	$.parse.get("tags", {where : { objectId: id }}, function(json) {
+        var results, html, pieceList;
+
+        results = json.results;
+        html = "";
+		pieceList = json.results[0].pieceIds;
+
+        if (results.length === 0) {
+            return false;
+        }
+		html += '<div class="tag-title"><h3> Tag: '+ json.results[0].tagName+'</h3></div>';
+		html += "<ul>";
+        pieceList.forEach(function(pieceId) {
+			console.log("pieceId", pieceId);
+			$.parse.get("pieces", {where : { objectId: pieceId }}, function(json) {
+				var results2;
+				results2 = json.results;
+				if (results2.length === 0) {
+					return false;
+				}
+				html += tmpl("piece_tmpl", results2[0]);
+				console.log("html", html);
+				piece_list.html(html);
+			});
+        });
+
+		myScroll.refresh();
+    });
+	
+  }
+
+  GLOBAL.pieces = {
+      init: init,
+      get: get
+  };
+
+})(window, jQuery, tmpl);
+
+$(function() {
+    pieces.init().get();
+});
+
+
+
